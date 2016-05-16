3e1088c0c72ccfdca20fb115ddb1b9dfacb6c5a0
Keith Moore <keith.m4@gmail.com>
2016-05-16 16:40:56 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Yik Yak Clone (Save the Hippo)
+
+In progress: a full stack PHP app. HTML, Compass, jQueryUI, Bootstrap, PHP, MeekroDB/MySQL.
---
+++ index.php
@@ -0,0 +1,70 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Save the Hippo</title>
+	<link rel="stylesheet" href="stylesheets/jquery-ui.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link rel="stylesheet" href="stylesheets/styles.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.3/jquery.min.js"></script>
+	<script src="javascripts/jquery-ui.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+	<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="navbar-header">
+	  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+	</div>
+
+	<!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	  <ul class="nav navbar-nav">
+		<li><a href="#">Home</a></li>
+	  </ul>
+	  <ul class="nav navbar-nav navbar-right">
+		<li><a href="#">Login</a></li>
+		<li><a href="#">Register</a></li>
+	  </ul>
+	</div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<div class="splash">
+	<div class="splash-inner">
+		<a href="#mission"><h1>Save the Hippo</h1></a>
+	</div>
+</div>
+
+
+<div class="container">
+	<div class="content-wrapper">
+		<div id="mission" class="mission">
+			<h2>Our Mission</h2>
+			<p>We are a society dedicated to saving the hippo. We have been active for over 3 years and embrace every chance we get to save one of these majestic creatures. This site is a means for you to share your hippo saving activity with other hippo lovers. Please sign up and tell us a bit about the last hippo you saved.</p>
+		</div>
+		<div id="posts" class="posts-wrapper">
+			<h2>Recent Posts</h2>
+			<p>Posts go here...</p>
+			<div class="post">
+				<h4>Test Post</h4>
+				<p>This is a post.</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
