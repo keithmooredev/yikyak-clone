c0b5432d7aa2a8f90a3b434f501c7dd7b9da6e80
Keith Moore <keith.m4@gmail.com>
2016-05-17 14:56:05 -0400

    outsourced header and navbar to includes/head.php

--- accounts.php
+++ accounts.php
@@ -4,10 +4,15 @@ if (isset($_GET['action'])){
 	$acct_action = $_GET['action'];
 
 	if ($acct_action == 'register'){
-		
-		
+		$username = $_POST['username'];
+		if ($_POST['password'] == $_POST['password2']){
+			$password = $_POST['password'];
+		} else {
+			$errorMessage = "The passwords do not match.";
+		}
+
 	} else if ($acct_action == 'login'){
-		
-		
+
+
 	}
 }
