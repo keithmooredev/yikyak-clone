d05d34282674d14dcba53d4abc50528bb48173a9
Keith Moore <keith.m4@gmail.com>
2016-05-18 21:04:40 -0400

    removed extraneous images and cleaned up logout styling

--- logout_process.php
+++ logout_process.php
@@ -4,6 +4,10 @@
 	session_destroy();
 	require_once('includes/header.php');
 	
-	print '<h2>Thank you for your support of the rocks.</h2>';
+	print '<div class="container">'.
+		'<div class="row">'.
+		'<div class="col-sm-12 text-center">'.
+		'<h2>You have been logged out. Thank you for supporting our cause!</h2>'.
+		'</div></div></div>';
 	
 	require_once('includes/footer.php');
